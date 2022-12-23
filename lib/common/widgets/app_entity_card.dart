d323ee0f3cac17ba7bc40ce5befbfcd819ae0291
Malte Nottmeyer <nottmey@users.noreply.github.com>
2022-12-23 01:07:29 +0100

    combine sketched card states into entity component

--- lib/common/widgets/app_entity_card.dart
+++ lib/common/widgets/app_entity_card.dart
@@ -1,12 +1,125 @@
+import 'package:climate_platform_ui/common/widgets/app_card.dart';
+import 'package:climate_platform_ui/common/widgets/app_text.dart';
 import 'package:climate_platform_ui/common/widgets/app_widget.dart';
+import 'package:climate_platform_ui/features/theming/models/text_style_preset.dart';
+import 'package:climate_platform_ui/features/theming/utils/context_theme_extension.dart';
+import 'package:climate_platform_ui/features/theming/utils/spacing_utils_extension.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 class AppEntityCard extends AppWidget {
-  const AppEntityCard({super.key});
+  final void Function()? onTab;
+
+  const AppEntityCard({super.key, this.onTab});
+
+  Widget buildCreating() {
+    return const Center(
+      child: Icon(Icons.add),
+    );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final dataState = useState<String?>(null);
+    final editingState = useState<bool>(false);
+
+    final theme = context.theme;
+    final data = dataState.value;
+    final editing = editingState.value;
+    final creating = data == null && !editing;
+
+    final preset = data == null && !editing
+        ? AppCardPreset.outlined
+        : AppCardPreset.elevated;
+
+    Widget buildEditing() {
+      // TODO try out https://pub.dev/packages/reactive_forms_generator
+      return ReactiveFormBuilder(
+        form: () => FormGroup({
+          'title': FormControl<String>(
+            value: data,
+            validators: [Validators.required],
+          )
+        }),
+        builder: (context, form, child) {
+          void save() {
+            dataState.value = form.value['title'] as String?;
+            editingState.value = false;
+          }
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ReactiveTextField<String>(
+                formControlName: 'title',
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                  border: OutlineInputBorder(gapPadding: 4),
+                ),
+                validationMessages: {
+                  ValidationMessage.required: (_) => 'Title must not be empty',
+                },
+                onSubmitted: (_) => save(),
+              ),
+              theme.spacedSizedBox(height: 1),
+              ReactiveFormConsumer(
+                builder: (context, form, child) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: theme.colorScheme.onPrimary,
+                      backgroundColor: theme.colorScheme.primary,
+                    ),
+                    onPressed: form.invalid ? null : save,
+                    child: const Text('Submit'),
+                  );
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    Widget buildDisplaying() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppText(
+            value: data ?? '',
+            preset: TextStylePreset.titleLarge,
+          ),
+        ],
+      );
+    }
+
+    return AppCard(
+      preset: preset,
+      onTab: editing
+          ? null
+          : data == null
+              ? () => editingState.value = true
+              : onTab,
+      actions: creating || editing
+          ? null
+          : [
+              AppCardAction(
+                label: 'Edit',
+                onSelected: () => editingState.value = true,
+              ),
+              AppCardAction(
+                label: 'Delete',
+                color: theme.errorColor,
+                onSelected: () {
+                  dataState.value = null;
+                },
+              ),
+            ],
+      child: creating
+          ? buildCreating()
+          : editing
+              ? buildEditing()
+              : buildDisplaying(),
+    );
   }
 }
--- lib/features/planetary_boundaries/pages/planet_overview_page.dart
+++ lib/features/planetary_boundaries/pages/planet_overview_page.dart
@@ -1,10 +1,6 @@
-import 'package:climate_platform_ui/common/widgets/app_card.dart';
+import 'package:climate_platform_ui/common/widgets/app_entity_card.dart';
 import 'package:climate_platform_ui/common/widgets/app_page_widget.dart';
-import 'package:climate_platform_ui/common/widgets/app_text.dart';
 import 'package:climate_platform_ui/features/navigation/widgets/main_top_bar.dart';
-import 'package:climate_platform_ui/features/theming/models/text_style_preset.dart';
-import 'package:climate_platform_ui/features/theming/utils/context_theme_extension.dart';
-import 'package:climate_platform_ui/features/theming/utils/spacing_utils_extension.dart';
 import 'package:climate_platform_ui/router.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -14,82 +10,16 @@ class PlanetOverviewPage extends AppPageWidget {
 
   @override
   List<Widget> buildSlivers(BuildContext context, WidgetRef ref) {
-    final theme = context.theme;
     return [
       const MainTopBar(title: 'Planetary boundaries'),
       // TODO sketch content
       // TODO content card grid
       SliverToBoxAdapter(
-        child: AppCard(
+        child: AppEntityCard(
+          // TODO connect with database
           onTab: () {
             context.go(overviewDetailsPath);
           },
-          actions: [
-            AppCardAction(
-              label: 'Edit',
-              onSelected: () {
-                // TODO
-              },
-            ),
-            AppCardAction(
-              label: 'Delete',
-              color: theme.errorColor,
-              onSelected: () {
-                // TODO
-              },
-            ),
-          ],
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              AppText(
-                value: 'Title',
-                preset: TextStylePreset.titleLarge,
-              ),
-            ],
-          ),
-        ),
-      ),
-      SliverToBoxAdapter(
-        child: AppCard(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextField(
-                decoration: const InputDecoration(
-                  labelText: 'Title',
-                  border: OutlineInputBorder(gapPadding: 4),
-                ),
-                controller: TextEditingController(text: ''),
-              ),
-              theme.spacedSizedBox(height: 1),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: theme.colorScheme.onPrimary,
-                  backgroundColor: theme.colorScheme.primary,
-                ),
-                onPressed: () {
-                  // TODO
-                },
-                child: const Text('Submit'),
-              ),
-              const ElevatedButton(
-                onPressed: null,
-                child: Text('Deactivated'),
-              ),
-            ],
-          ),
-        ),
-      ),
-      SliverToBoxAdapter(
-        child: AppCard(
-          preset: AppCardPreset.outlined,
-          onTab: () {
-            // TODO
-          },
-          child: const Center(
-            child: Icon(Icons.add),
-          ),
         ),
       ),
     ];
