0369c0a4362ffcb2eb36c8266075b078263034f9
Malte Nottmeyer <nottmey@users.noreply.github.com>
2023-02-17 19:24:36 +0100

    docs: add comment for entity cache notifier

--- lib/common/notifiers/entity_cache_notifier.dart
+++ lib/common/notifiers/entity_cache_notifier.dart
@@ -1,9 +1,16 @@
 import 'package:climate_platform_ui/common/models/entity.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+/// Device local source of truth for entity data. Used for optimistic updates,
+/// and to pass already present data into entity provider families, which only
+/// use an id as argument.
 class EntityCacheNotifier<T extends Entity>
     extends StateNotifier<Map<String, T>> {
   EntityCacheNotifier() : super({});
+
+  T? get(String id) {
+    return state[id];
+  }
 
   void add(List<T> values) {
     // TODO make efficient
@@ -14,10 +21,6 @@ class EntityCacheNotifier<T extends Entity>
           if (value.id != null) value.id!: value,
       },
     };
-  }
-
-  T? get(String id) {
-    return state[id];
   }
 
   void update(String id, T value) {
