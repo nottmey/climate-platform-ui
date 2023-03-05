282bebc923866a885b9ddd160b386a52abf61715
Malte Nottmeyer <nottmey@users.noreply.github.com>
2023-03-05 23:01:34 +0100

    fix: use https api for entity provider

--- lib/features/database_browser/providers/entity_provider_family.dart
+++ lib/features/database_browser/providers/entity_provider_family.dart
@@ -1,11 +1,10 @@
 import 'package:climate_platform_ui/api/api.graphql.dart';
-import 'package:climate_platform_ui/api/utils/stream.dart';
+import 'package:climate_platform_ui/api/utils/execute.dart';
 import 'package:climate_platform_ui/api/utils/throw_not_found_error.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final entityProviderFamily = StreamProvider.autoDispose
-    .family<EntityMixin, GetEntityArguments>((ref, arguments) {
-  return stream(GetEntityQuery(variables: arguments))
-      .map((data) => data.getEntity)
-      .map(throwNotFoundError);
+final entityProviderFamily = FutureProvider.autoDispose
+    .family<EntityMixin, GetEntityArguments>((ref, arguments) async {
+  final result = await execute(GetEntityQuery(variables: arguments));
+  return throwNotFoundError(result.getEntity);
 });
