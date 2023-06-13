78df53ecdb7d645e946280ae977b9ae8783625b7
Malte Nottmeyer <nottmey@users.noreply.github.com>
2023-06-13 23:25:58 +0200

    fix: add findRichText to app description test

--- test/unit/common/widgets/app_description_block_test.dart
+++ test/unit/common/widgets/app_description_block_test.dart
@@ -4,12 +4,13 @@ import 'package:climate_platform_ui/common/widgets/app_description_block.dart';
 import 'package:climate_platform_ui/common/widgets/app_error.dart';
 import 'package:climate_platform_ui/common/widgets/app_loading.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+// widget testing demo
 void main() {
-  // widget testing demo
-  testWidgets('$AppDescriptionBlock', (tester) async {
+  testWidgets('$AppDescriptionBlock with direct state', (tester) async {
     await tester.pumpWidget(
       ProviderScope(
         child: Directionality(
@@ -21,8 +22,10 @@ void main() {
         ),
       ),
     );
-    expect(find.text('direct'), findsOneWidget);
-
+    expect(find.byType(Markdown), findsOneWidget);
+    expect(find.text('direct', findRichText: true), findsOneWidget);
+  });
+  testWidgets('$AppDescriptionBlock with stream', (tester) async {
     final controller = StreamController<String?>();
     final streamProvider = StreamProvider((ref) => controller.stream);
     await tester.pumpWidget(
@@ -39,7 +42,8 @@ void main() {
     expect(find.byType(AppLoading), findsOneWidget);
     controller.sink.add('state change');
     await tester.pump();
-    expect(find.text('state change'), findsOneWidget);
+    expect(find.byType(Markdown), findsOneWidget);
+    expect(find.text('state change', findRichText: true), findsOneWidget);
     controller.sink.addError(Exception('error'));
     await tester.pump();
     expect(find.byType(AppError), findsOneWidget);
